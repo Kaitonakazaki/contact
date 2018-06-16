a08d03471a0d59ca205f5f2b7eb4386c08af51f3
kaitonakazaki <kaitonakazaki0418@gmail.com>
2018-06-16 06:30:41 +0000

    first commit

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,52 @@
+class ContactsController < ApplicationController
+  before_action :set_blog, only: [:show, :edit, :update, :destroy]
+  def index
+    @contacts = Contact.all
+    # binding.pry
+    # raise
+  end
+ 
+  def new
+    @contact = Contact.new
+  end
+  
+  def create
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      redirect_to new_contact_path, notice: "ブログを作成しました！"
+    else 
+      render :new
+    end
+  end
+  
+  def show
+    @contact = Contact.find(params[:id])
+  end
+  
+  def edit
+    @contact = Contact.find(params[:id])
+  end
+  
+  def update
+    @contact = Contact.find(params[:id])
+    if @contact.update(blog_params)
+      redirect_to contacts_path, notice: "ブログを編集しました！"
+    else 
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    @contact.destroy
+    redirect_to contacts_path, notice:"ブログを削除しました"
+  end
+  
+  private
+  def contact_params
+    params.require(:contact).permit(:name,:email,:content)
+  end
+  
+  def set_blog
+      @contact = Contact.find(params[:id])
+  end
+end
